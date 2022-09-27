f848c2cd849a17085c1f175d275a8433b008c0a2
henryfeng1008 <henryfeng1008@126.com>
2022-09-27 11:07:36 +0800

    Project0 finished

--- project0/shop.py
+++ project0/shop.py
@@ -1,25 +1,31 @@
 # shop.py
 # -------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
 
 class FruitShop:
 
     def __init__(self, name, fruitPrices):
         """
             name: Name of the fruit shop
-            
-            fruitPrices: Dictionary with keys as fruit 
-            strings and prices for values e.g. 
-            {'apples':2.00, 'oranges': 1.50, 'pears': 1.75} 
+
+            fruitPrices: Dictionary with keys as fruit
+            strings and prices for values e.g.
+            {'apples':2.00, 'oranges': 1.50, 'pears': 1.75}
         """
         self.fruitPrices = fruitPrices
         self.name = name
-        print 'Welcome to %s fruit shop' % (name)
-        
+        print('Welcome to %s fruit shop' % (name))
+
     def getCostPerPound(self, fruit):
         """
             fruit: Fruit string
@@ -27,22 +33,28 @@ class FruitShop:
         is in our inventory or None otherwise
         """
         if fruit not in self.fruitPrices:
-            print "Sorry we don't have %s" % (fruit)
             return None
         return self.fruitPrices[fruit]
-        
+
     def getPriceOfOrder(self, orderList):
         """
             orderList: List of (fruit, numPounds) tuples
-            
-        Returns cost of orderList. If any of the fruit are  
-        """ 
-        totalCost = 0.0             
+
+        Returns cost of orderList, only including the values of
+        fruits that this fruit shop has.
+        """
+        totalCost = 0.0
         for fruit, numPounds in orderList:
             costPerPound = self.getCostPerPound(fruit)
             if costPerPound != None:
                 totalCost += numPounds * costPerPound
         return totalCost
-    
+
     def getName(self):
         return self.name
+
+    def __str__(self):
+        return "<FruitShop: %s>" % self.getName()
+
+    def __repr__(self):
+        return str(self)
--- project0/shopSmart.py
+++ project0/shopSmart.py
@@ -1,10 +1,16 @@
 # shopSmart.py
 # ------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
 
 """
 Here's the intended output of this script, once you fill it in:
@@ -14,30 +20,35 @@ Welcome to shop2 fruit shop
 For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
 For orders:  [('apples', 3.0)] best shop is shop2
 """
-
+from __future__ import print_function
+from cmath import inf
 import shop
+
 
 def shopSmart(orderList, fruitShops):
     """
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
-    """    
+    """
     "*** YOUR CODE HERE ***"
-    cost1 = fruitShops[0].getPriceOfOrder(orderList)
-    cost2 = fruitShops[1].getPriceOfOrder(orderList)
-    if cost1 > cost2:
-        return fruitShops[1]
-    else:
-        return fruitShops[0] 
-    
+    minCost = inf
+    result = None
+    for shop in fruitShops:
+        cost = shop.getPriceOfOrder(orderList)
+        if cost < minCost:
+            result = shop
+            minCost = cost
+    return result
+
+
 if __name__ == '__main__':
-  "This code runs when you invoke the script from the command line"
-  orders = [('apples',1.0), ('oranges',3.0)]
-  dir1 = {'apples': 2.0, 'oranges':1.0}
-  shop1 =  shop.FruitShop('shop1',dir1)
-  dir2 = {'apples': 1.0, 'oranges': 5.0}
-  shop2 = shop.FruitShop('shop2',dir2)
-  shops = [shop1, shop2]
-  print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
-  orders = [('apples',3.0)]
-  print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    orders = [('apples', 3.0)]
+    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
